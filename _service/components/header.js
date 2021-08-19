63b628bd7c3da01bbda59755f34a6bb78bd29561
vencax <vencax@centrum.cz>
2021-08-19 09:07:57 +0200

    update

--- _service/components/header.js
+++ _service/components/header.js
@@ -34,11 +34,17 @@ export default {
 
     <div class="navbar-end">
       <div class="navbar-item">
-        <div class="buttons">
+        <span v-if="this.$store.getters.userLogged">
+          {{ $store.state.user.CurrentFamilyName }} {{ $store.state.user.CurrentGivenName }}
+          <a class="button is-primary" href="/nia/logout">
+            Odhlásit
+          </a>
+        </span>
+        <div v-else class="buttons">
           <a class="button is-primary" href="https://www.eidentita.cz/Home">
             <strong>Zaregistrovat</strong>
           </a>
-          <a class="button is-light" href="https://www.eidentita.cz/Home">
+          <a class="button is-light" href="/nia/login">
             Příhlásit se
           </a>
         </div>
